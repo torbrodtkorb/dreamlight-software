9340ef85cce11272838300115db3e5b380d09d1a
Tor Brodtkorb <tor.brodtkorb98@gmail.com>
2020-07-12 00:52:33 -1100

    First commit

---
+++ dreamlight software/main.c
@@ -0,0 +1,21 @@
+/*
+ * dreamlight software.c
+ *
+ * Created: 7/12/2020 12:46:42 AM
+ * Author : Tor Brodtkorb
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
