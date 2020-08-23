820a7ee442e4fae56282d36539bdc01f199984c1
Tor Brodtkorb <tor.brodtkorb98@gmail.com>
2020-08-23 02:14:55 +0200

    Started on spi driver

--- dreamlight software/Drivers/spi.c
+++ dreamlight software/Drivers/spi.c
@@ -1,3 +1,8 @@
 /// Copyright (C) Tor Brodtkorb
 
 #include "spi.h"
+
+void spi_init(u8 data_bit){
+
+}
+
--- dreamlight software/Drivers/spi.h
+++ dreamlight software/Drivers/spi.h
@@ -3,6 +3,15 @@
 #ifndef SPI_H
 #define SPI_H
 
+struct spi_desc{
+	u8 data_bit;
+	u8 bit_rate;
+	u8 clock_phase;
+	
+	};
+
+
+void spi_init(Spi* hardware, struct spi_desc* desc);
 
 
 #endif
