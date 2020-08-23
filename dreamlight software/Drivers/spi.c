bba593409bc7921a8cfede9f1dbdec8f0ff58058
Tor Brodtkorb <tor.brodtkorb98@gmail.com>
2020-08-23 19:00:39 +0200

    Done with spi driver, not tested

--- dreamlight software/Drivers/spi.c
+++ dreamlight software/Drivers/spi.c
@@ -2,7 +2,39 @@
 
 #include "spi.h"
 
-void spi_init(Spi* hardware, struct spi_desc* desc)
+void spi_init(Spi* hw, struct spi_desc* desc)
 {
+	/*
+	 * disabled local loopback path
+	 * disabled wait date read before transfer
+	 * disabled chip select decode
+	 * Enabled Fixed peripheral
+	 */
+	u32 reg =	(desc->delay_between_chip_select << 24) |
+				(desc->fixed_cs << 16) |
+				/* Disable mode fault detection */
+				(1 << 4) |
+				(desc->master_mode_enable << 0);
+				
+	hw->SPI_MR = reg;
+	
+	reg =	(desc->delay_between_consecutive_transfers << 24) |
+			(desc->delay_between_falling_edge << 16) |
+			(desc->bit_rate << 8) |
+			(desc->bit_per_transfer << 4) |
+			/* The Peripheral Chip Select Line does not rise after the last transfer is achieved */
+			(1 << 3) |
+			(desc->clock_phase << 1) |
+			(desc->clock_polarity << 0);
+			
+	hw->SPI_CSR[desc->chip_select];
+	
+	/* Enable spi */
+	hw->SPI_CR = (1 << 0);
+}
 
+void spi_transmit_8_bit(Spi* hw, u8 data)
+{
+	while (!(hw->SPI_SR & (1 << 9)));
+	hw->SPI_TDR = data;
 }
--- dreamlight software/Drivers/spi.h
+++ dreamlight software/Drivers/spi.h
@@ -6,12 +6,60 @@
 #include "sam.h"
 #include "types.h"
 
-struct spi_desc {
-	u8 data_bit;
-	u8 bit_rate;
-	u8 clock_phase;
+enum spi_fixed_chip_select {
+	SPI_FIXED_CS_0		= 0b1110,
+	SPI_FIXED_CS_1		= 0b1101,
+	SPI_FIXED_CS_2		= 0b1011,
+	SPI_FIXED_CS_3		= 0b0111,
+	SPI_FIXED_CS_NONE	= 0b111
+	};
+	
+enum spi_bit_per_transfer {
+	SPI_8_BIT,
+	SPI_9_BIT,
+	SPI_10_BIT,
+	SPI_11_BIT,
+	SPI_12_BIT,
+	SPI_13_BIT,
+	SPI_14_BIT,
+	SPI_15_BIT,
+	SPI_16_BIT,
+	};
+	
+enum spi_chip_select {
+	SPI_CHIP_SELECT_0,
+	SPI_CHIP_SELECT_1,
+	SPI_CHIP_SELECT_2,
+	SPI_CHIP_SELECT_3
+	};
+	
+enum spi_clock_phase {
+	SPI_CLOCK_CAPTURE_ON_TRAILING,
+	SPI_CLOCK_CAPTURE_ON_LEADING
 	};
 
-void spi_init(Spi* hardware, struct spi_desc* desc);
+enum spi_clock_polarity {
+	SPI_CLOCK_INACTIVE_LOW,
+	SPI_CLOCK_INACTIVE_HIGH
+	};
+
+struct spi_desc {
+	u8 delay_between_chip_select;
+	spi_fixed_chip_select fixed_cs;
+	u8 master_mode_enable : 1;
+	
+	u8 delay_between_consecutive_transfers;
+	u8 delay_between_falling_edge;
+	u8 bit_rate;
+	spi_bit_per_transfer bit_per_transfer;
+	enum spi_clock_phase clock_phase;
+	enum spi_clock_polarity clock_polarity;
+	spi_chip_select chip_select;
+	};
+
+void spi_init(Spi* hw, struct spi_desc* desc);
+
+void spi_transmit_8_bit(Spi* hw, u8 data);
+
 
 #endif
